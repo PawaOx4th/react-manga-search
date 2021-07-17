faf15c1bc0a84ae62558bdda165b0ad48f0e692d
pawaox4th <pawaox4th@gmail.com>
2021-07-17 22:43:52 +0700

    Add tailwind.css success,

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import Avatar from "./assets/avatar.png";
+import Avatar from "./assets/images/avatar.png";
+import ButtonCustom from "./components/Button/Button";
 import "./App.css";
 
 function App() {
@@ -15,34 +16,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={Avatar} alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => handleCount()}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <ButtonCustom />
       </header>
     </div>
   );
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ButtonCustom() {
+  return (
+    <div className="text-white bg-green-500 px-5 py-2 rounded-lg cursor-pointer transition-all hover:bg-green-300">
+      Click
+    </div>
+  );
+}
+
+export default ButtonCustom;
