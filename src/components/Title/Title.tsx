a6263e38de9d8898a0f9f84c0d381b72b7f14f22
pawaox4th <pawaox4th@gmail.com>
2021-07-18 23:10:39 +0700

    Add react-spinners

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from "react";
 import { useFetch } from "@/hook/useFetch";
+import { MangaAPIResponseType, MangaType } from "@/types/mangaType";
+
 interface Props {
   children: ReactNode;
 }
@@ -7,10 +9,18 @@ interface Props {
 export default function Title(props: Props) {
   const { children } = props;
 
-  const { data, error, isLoad } = useFetch(
+  const { data, error, isLoad } = useFetch<MangaAPIResponseType>(
     { method: "GET" },
     "https://api.jikan.moe/v3/search/manga?q=Fate/Zero"
   );
 
-  return <div>{children}</div>;
+  return (
+    <div>
+      <code>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
+        exercitationem in inventore odio ea vel soluta possimus ab nostrum.
+        Dolorum.
+      </code>
+    </div>
+  );
 }
