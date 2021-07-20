994ba8a6e0ac664364a6f8d6aea29afe5bd03dde
pawaox4th <pawaox4th@gmail.com>
2021-07-20 23:03:25 +0700

    Clear file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
-import Avatar from "./assets/images/avatar.png";
-import ContentComponent from "./components/Content/Content";
-import TitleComponent from "./components/Title/Title";
-
-interface SampleContextInterface {
-  count: number;
-  increment: Function;
-}
-
-export const SampleContext = React.createContext<SampleContextInterface | null>(
-  null
-);
 
 function App() {
   const [state, setState] = useState(0);
 
-  return (
-    <div className="App">
-      <SampleContext.Provider value={{ count: state, increment: setState }}>
-        <div className="w-8/12 my-0 mx-auto ">
-          <img src={Avatar} alt="logo" className="h-profile-md" />
-          <TitleComponent />
-          <ContentComponent></ContentComponent>
-        </div>
-      </SampleContext.Provider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
